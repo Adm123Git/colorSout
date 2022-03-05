0a721dba861a015302dfcf5f3ac50efb32fdf6ba
Adm123 <adm123@mail.ru>
2022-03-05 18:57:57 +0300

    fix gradle for publishing

--- src/main/java/ru/adm123/colorSout/Color.java
+++ src/main/java/ru/adm123/colorSout/Color.java
@@ -5,13 +5,37 @@ package ru.adm123.colorSout;
  */
 public enum Color {
 
+    /**
+     * Черный
+     */
     BLACK("\u001B[30m"),
+    /**
+     * Красный
+     */
     RED	("\u001B[31m"),
+    /**
+     * Зеленый
+     */
     GREEN("\u001B[32m"),
+    /**
+     * Желтый
+     */
     YELLOW("\u001B[33m"),
+    /**
+     * Синий
+     */
     BLUE("\u001B[34m"),
+    /**
+     * Фиолетовый
+     */
     PURPLE("\u001B[35m"),
+    /**
+     * Сине-зеленый
+     */
     CYAN("\u001B[36m"),
+    /**
+     * Белый
+     */
     WHITE("\u001B[37m");
 
     private final String colorCode;
@@ -20,6 +44,10 @@ public enum Color {
         this.colorCode = colorCode;
     }
 
+    /**
+     * Возвращает цвет в "понятном" для метода {@code System.out.print()} виде
+     * @return строковое значение цвета
+     */
     public String getColorCode() {
         return colorCode;
     }
--- src/main/java/ru/adm123/colorSout/ColorSout.java
+++ src/main/java/ru/adm123/colorSout/ColorSout.java
@@ -7,16 +7,46 @@ public final class ColorSout {
 
     private static final String COLOR_RESET = "\u001B[0m";
 
-    public static void print(Color color,
-                             String text) {
-        text = text == null ? "" : text;
-        System.out.print(color.getColorCode() + text + COLOR_RESET);
+    /**
+     * Класс - утилитный, публичный конструктор ему не нужен
+     */
+    private ColorSout() {
     }
 
+    /**
+     * Аналог метода {@code System.out.print(String)} с той разницей, что
+     * текст выводится в заданном цвете
+     *
+     * @param color цвет текста, заданный константой {@link Color}
+     * @param text  текст для вывода
+     */
+    public static void print(Color color,
+                             String text) {
+        textOut(color, text, false);
+    }
+
+    /**
+     * Аналог метода {@code System.out.println(String)} с той разницей, что
+     * текст выводится в заданном цвете
+     *
+     * @param color цвет текста, заданный константой {@link Color}
+     * @param text  текст для вывода
+     */
     public static void println(Color color,
                                String text) {
+        textOut(color, text, true);
+    }
+
+    private static void textOut(Color color,
+                                String text,
+                                boolean goNextString) {
         text = text == null ? "" : text;
-        System.out.println(color.getColorCode() + text + COLOR_RESET);
+        System.out.print(color.getColorCode());
+        System.out.print(text);
+        System.out.print(COLOR_RESET);
+        if (goNextString) {
+            System.out.println();
+        }
     }
 
 }
